32254d0482e2dae611e632d05bdde4d58379d9d6
Oscolok <oscolok7@gmail.com>
2020-08-05 21:09:46 +0300

    part 2 is done

--- src/redux/actions/itemsAction.js
+++ src/redux/actions/itemsAction.js
@@ -1,10 +1,14 @@
 import { ADD_USER, DELETE_USER } from "../constants/constants";
+import { createAction } from "@reduxjs/toolkit";
 
-export const userAdd = (value) => ({
-  type: ADD_USER,
-  payload: value,
-});
-export const userDelete = (id) => ({
-  type: DELETE_USER,
-  payload: id,
-});
+export const userAdd = createAction(ADD_USER);
+export const userDelete = createAction(DELETE_USER);
+
+// export const userAdd = (value) => ({
+//   type: ADD_USER,
+//   payload: value,
+// });
+// export const userDelete = (id) => ({
+//   type: DELETE_USER,
+//   payload: id,
+// });
--- src/redux/reducers/itemsReducer.js
+++ src/redux/reducers/itemsReducer.js
@@ -1,14 +1,23 @@
-import { ADD_USER, DELETE_USER } from "../constants/constants";
+// import { ADD_USER, DELETE_USER } from "../constants/constants";
 
-const initialState = [];
+// const initialState = [];
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_USER:
-      return [...state, action.payload];
-    case DELETE_USER:
-      return state.filter((item) => item.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_USER:
+//       return [...state, action.payload][...state, action.payload];
+//     case DELETE_USER:
+//       return state.filter((item) => item.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+import { createReducer } from "@reduxjs/toolkit";
+import { userAdd, userDelete } from "../actions/itemsAction";
+
+export default createReducer([], {
+  [userAdd]: (state, action) => [...state, action.payload],
+  [userDelete]: (state, action) =>
+    state.filter((item) => item.id !== action.payload),
+});
